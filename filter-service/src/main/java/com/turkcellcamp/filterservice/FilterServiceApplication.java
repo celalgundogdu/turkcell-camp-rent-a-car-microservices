d3ec428c39e3eb05206f3ca65b39e2610372ed56
Celal Gündoğdu <celalgundogdu42@gmail.com>
2023-05-08 00:50:39 +0300

    add discovery server

--- filter-service/src/main/java/com/turkcellcamp/filterservice/FilterServiceApplication.java
+++ filter-service/src/main/java/com/turkcellcamp/filterservice/FilterServiceApplication.java
@@ -2,8 +2,10 @@ package com.turkcellcamp.filterservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class FilterServiceApplication {
 
 	public static void main(String[] args) {
